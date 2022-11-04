e8adf0998b1d51153e6a6a69624b9c680eebd0d7
Sukhrob <khojiakbarmurodo05v@gmail.com>
2022-11-04 11:02:26 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22, 45, 23]
+
+
+if (arr.filter(Number).length > 5) {
+    console.log('good');
+} else {
+    console.log('not good');
+}
+
+
+
+
+
+let arrTwo =
+
+
+
+    [
+        [
+            {
+                a: { price: 20 }
+            }
+        ],
+        [
+        {
+            a: { price: 35 }
+        }
+        ],
+        [
+        {
+            a: { price: 44 }
+        }
+        ]
+    ]
+
+
+let total = 0
+
+
+console.log(arrTwo.filter(item => {
+    arrTwo.sort((a, b), a + b)
+}));
+
